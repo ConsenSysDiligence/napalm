f86d7b0262f36a04c06ca479ee70fc9e04724b3a
Joran Honig <joran.honig@gmail.com>
2024-02-21 13:30:29 -0600

    :sparkles: testing your detection modules

--- napalm/cli/dev/testing.py
+++ napalm/cli/dev/testing.py
@@ -3,9 +3,18 @@ from dataclasses import dataclass
 from pathlib import Path
 import re
 from enum import Enum
+
+
+from typing import List
 from napalm.storage import get_storage_provider
 from napalm.tool.run import run_tools
+from napalm.utils.workflow import get_collections_for_workflow
+from napalm.package.collection_manager import CollectionManager
+from pydantic_sarif.model import StaticAnalysisResultsFormatSarifVersion210JsonSchema as Report
+from pydantic_sarif.model import Result
 
+from toolz import groupby
+from rich.console import Console
 
 class TestType(Enum):
     detect = "napalm-detect"
@@ -20,6 +29,8 @@ class ExpectedFinding:
     line: int  # the line number where we've added the annotation of the expected finding
     type: TestType  # the type of finding we expect to have (detect / ok / wip)
     detector_id: str  # the id of the detector that we expect to have a finding for
+
+    discovered: bool = False  # whether we've found a matching finding in the report
 
 
 def _expected_findings(directory_path: Path):
@@ -51,13 +62,102 @@ def _expected_findings(directory_path: Path):
 
 def _run_tools(directory_path: Path, ai_filter: bool = False):
     # the directory is a foundry project and should work with all supported tools
-    collections = []
+    collection_manager = CollectionManager()
+    collections = get_collections_for_workflow(get_storage_provider('pickle'), 'all')
+    collections = list(
+        filter(
+            lambda a: a is not None,
+            map(lambda c: collection_manager.get(c), collections),
+        )
+    )
+
     semgrep_arguments = []
-    workflow = None
+    workflow = 'all'
     storage = get_storage_provider("pickle")
-    run_tools(directory_path, collections, semgrep_arguments, workflow, storage, ai_filter)
+
+
+    return run_tools(directory_path, collections, semgrep_arguments, workflow, storage, ai_filter)
+
+
+def _match_expectation_to_finding(expectation: ExpectedFinding, finding: Result):
+    rule_id = finding.ruleId
+    if re.match(r"\d-\d-.*", rule_id):
+        rule_id = re.match(r"\d-\d-(.*)", rule_id).group(1)
+
+    if not rule_id == expectation.detector_id:
+        return False
+
+    for location in finding.locations:
+        if location.physicalLocation.artifactLocation.uri != str(expectation.file):
+            continue
+        if location.physicalLocation.region.startLine not in (expectation.line, expectation.line + 1):
+            continue
+        return True
+
+    return False
+
+
+def _validate_findings(expected_findings: List[ExpectedFinding], report: Report):
+    grouped_expectations = groupby(lambda expected: expected.detector_id, expected_findings)
+
+    for run in report.runs:
+        for finding in run.results:
+            rule_id = finding.ruleId
+            if re.match(r"\d-\d-.*", rule_id):
+                rule_id = re.match(r"\d-\d-(.*)", rule_id).group(1)
+
+            expectations = grouped_expectations.get(rule_id)
+
+            if not expectations:
+                continue
+
+            for expectation in expectations:
+                if _match_expectation_to_finding(expectation, finding):
+                    expectation.discovered = True
+
+    for expectation in expected_findings:
+        if (expectation.type == TestType.detect or expectation.type == TestType.wip_detect) and not expectation.discovered:
+            yield expectation
+        elif (expectation.type == TestType.ok or expectation.type == TestType.wip_ok) and expectation.discovered:
+            yield expectation
+
+
+def _report(expectations: List[ExpectedFinding], missed_expectations: List[ExpectedFinding]):
+    console = Console()
+
+    console.print(f"Ran [green]{len(expectations)}[/green] tests.")
+
+    if not missed_expectations:
+        console.print("All tests passed!")
+        return
+    if missed_expectations:
+        console.print(f"Missed [red]{len(missed_expectations)}[/red] tests!")
+
+    for expectation in missed_expectations:
+        # with color highlighting
+        console.print(
+            f"[red]{expectation.file}:{expectation.line} - {expectation.detector_id}[/red]"
+        )
 
 
 @click.command(help="Run tests for this project")
-def test():
-    pass
+@click.option("--directory", type=click.Path(), default="./test/corpus")
+def test(directory):
+    directory = Path(directory)
+
+    console = Console()
+    if not directory.exists() or not directory.is_dir():
+        console.print(f"Directory {directory} does not exist or is not a directory")
+        exit(1)
+
+    expectations = _expected_findings(directory)
+    console.print(f"Running [green]{len(expectations)}[/green] tests!")
+
+    report = _run_tools(directory)
+
+    missed_expectations = _validate_findings(expectations, report)
+
+    _report(expectations, list(missed_expectations))
+
+    if any(expectation.type in (TestType.detect, TestType.ok) for expectation in missed_expectations):
+        raise click.Abort("Tests failed!")
